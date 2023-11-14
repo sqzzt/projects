1c9d4e62d1e54be147813cf339a33b83323a81d9
sqzzt <midominguezro@hotmail.com>
2023-11-14 22:02:26 +0100

    udpate final exercise

---
+++ S8/3.async-await/exercise-1.js
@@ -0,0 +1,14 @@
+// Convierte la siguiente promesa para esperar a ejecutar el console usando async-await.
+
+const runTimeOut = async () => {
+  const promise = new Promise((resolve) => {
+    setTimeout(function () {
+      resolve();
+    }, 2000);
+  });
+
+  await promise;
+  console.log("Time out!");
+};
+
+runTimeOut();
